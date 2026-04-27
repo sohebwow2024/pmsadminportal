57862098f164ec3d8d37e10f83959e6ccb63fc41
Azim <azimaga908@gmail.com>
2026-04-27 14:09:59 +0530

    changes

--- src/navigation/vertical/hotelPannel.js
+++ src/navigation/vertical/hotelPannel.js
@@ -7,7 +7,7 @@ export default [
   {
     id: 'reports',
     title: 'Client Manager',
-    icon: <TbReportSearch size={20} />,
+    icon: <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-users"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M5 7a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /><path d="M21 21v-2a4 4 0 0 0 -3 -3.85" /></svg>,
     navLink: '/addClient',
     // children: [
     //   // {
--- src/navigation/vertical/promotions.js
+++ src/navigation/vertical/promotions.js
@@ -19,7 +19,7 @@ export default [
   {
     id: 'Licences',
     title: 'Plan Master',
-    icon: <BiBuildingHouse size={20} />,
+    icon: <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-clipboard-list"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" /><path d="M9 5a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2" /><path d="M9 12l.01 0" /><path d="M13 12l2 0" /><path d="M9 16l.01 0" /><path d="M13 16l2 0" /></svg>,
     navLink: '/pannelmaster',
     children: [
       {
@@ -64,4 +64,3 @@ export default [
     ]
   }
 ]
-
